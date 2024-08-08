b8a42d92f3f370e82f3cbe7deb88083896d202b3
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-08-08 18:44:24 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,3 @@
-/* Change this file to get your personal Porfolio */
-
-// Website related settings
 const settings = {
   isSplash: false, // Change this to false if you don't want Splash screen.
 };
@@ -41,12 +38,6 @@ const socialMediaLinks = [
     link: "https://www.linkedin.com/in/iamnkr67/",
     fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
-  },
-  {
-    name: "YouTube",
-    link: "https://www.youtube.com/@iamnkr67",
-    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
-    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
   },
   {
     name: "Gmail",

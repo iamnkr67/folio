0edfa229e5887aed499a533ca7554ae2780b86c2
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-08-09 11:36:00 +0530

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -544,10 +544,10 @@ const experience = {
           company: "YAMAHA Motors India",
           company_url: "https://www.yamaha-motor-india.com/",
           logo_path: "yamaha_logo.png",
-          duration: "May 2019 - Sept 2019",
-          location: "Gurgaon, Haryana",
+          duration: "May 2022",
+          location: "Chitkara University, Himachal Pradesh",
           description:
-            "I have worked on project of predicting freight rates based on previous data. There were two objectives: (1) To build a forecasting engine to predict daily freight rates. (2) To embed feature in the model which can explain the seasonal major changes in freight rate based on regions and locations. I have closely worked with deep learning models in combination with statistical methods to create solution for this. At the end of internship, I had created model deployed on AWS EC2 with the use of Kafka stream jobs, ElasticSearch and PostgreSQL.",
+            "I participated in the Yamaha AI 2.0 Hackathon as a first-year student, where I gained valuable insights into AI and ML by creating Grapes instances. Although I didn't win, but the experience significantly broadened my understanding of these technologies (AI/ML).",
           color: "#ee3c26",
         },
         // {

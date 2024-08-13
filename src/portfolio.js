238513929e527932a48e2af674c9e1224f0b04ab
Nitesh Kumar <niteshkumaro7@hotmail.com>
2024-08-13 09:49:05 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -502,6 +502,7 @@ const contactPageData = {
   contactSection: {
     title: "Contact Me",
     profile_image_path: "avatar.png",
+    resume_link : "https://drive.google.com/file/d/1FyYVYIPKQdo6GjlgtpJ36cQRB7ovqfHV/view?usp=drivesdk"
     description:
       "I am available on almost every social media. You can message me, and I will reply within 24 hours. I can help you with ML, AI, React, Android, and Cloud.",
   },
